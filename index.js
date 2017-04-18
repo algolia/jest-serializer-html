3470ddde015f4f12dc27f12166d384693d2582ec
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-04-18 11:58:59 +0200

    feat(html-format): better default HTML formatting that ameliorates diff readability
    Closes: #4

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-var beautify = require('js-beautify').html;
+var toDiffableHtml = require('diffable-html');
 
 module.exports = {
   test(object) {
     return typeof object === 'string' && object[0] === '<';
   },
   print(val) {
-    return beautify(val, {
-      unformatted: [],
-      wrap_attributes: 'force-aligned',
-      indent_size: 2
-    });
+    return toDiffableHtml(val);
   },
 };
